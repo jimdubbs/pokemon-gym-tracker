e76312b5d216b06e14ad38053b6eabd052b7c0a5
Mark Murphy <markmurphy@gmail.com>
2016-07-20 17:42:43 -0230

    Markers work, gym data

--- public/layout/master.controller.js
+++ public/layout/master.controller.js
@@ -5,10 +5,11 @@
         .module('main')
         .controller('MasterViewController', MasterViewController);
 
-    MasterViewController.$inject = ['uiGmapGoogleMapApi'];
-    function MasterViewController(uiGmapGoogleMapApi) {
+    MasterViewController.$inject = ['uiGmapGoogleMapApi', 'GymService'];
+    function MasterViewController(uiGmapGoogleMapApi, gymService) {
         var vm = this;
         vm.uiGmapGoogleMapApi = uiGmapGoogleMapApi;
+        vm.gymService = gymService;
         vm.map = {
             center: {
                 latitude: 47.5605644,
@@ -17,13 +18,30 @@
             zoom: 13
         };
         vm.map.markers = [];
-
+        vm.gyms = [];
+        vm.idCounter = 1;
 
         activate();
 
         ////////////////
 
+
         function activate() {
+
+            vm.gymService.getGymData()
+                .then(function (response) {
+                    response.forEach(function (gym) {
+                        vm.idCounter++;
+                        vm.map.markers.push({
+                            id: vm.idCounter,
+                            latitude: gym.coords.latitude,
+                            longitude: gym.coords.longitude
+                        })
+
+                        vm.gyms.push(gym);
+                    })
+                });
+
             function showPosition(position) {
                 vm.map.markers.push({
                     id: 1,
@@ -35,8 +53,6 @@
                     latitude: position.coords.latitude,
                     longitude: position.coords.longitude
                 };
-
-
             }
 
             navigator.geolocation.getCurrentPosition(showPosition);
